b7db7d5f70e625e0d97bcf540a02a60529b86b44
Chandani <nittianchandani@gmail.com>
2026-08-09 00:58:16 +0530

    Hollow square pattern printing

---
+++ Java_DSA/src/Pattern/HollowSquare.java
@@ -0,0 +1,27 @@
+
+package Pattern;
+
+public class HollowSquare {
+
+    public static void main(String[] args) {
+        int n = 4;
+
+        for (int row = 1; row <= n; row++) {
+
+            // For each row -> 6 columns
+            for (int col = 1; col <= 6; col++) {
+
+                // Print star on first/last row
+                // or first/last column
+                if (row == 1 || row == n || col == 1 || col == 6) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+
+            // Move to next row
+            System.out.println();
+        }
+    }
+}
