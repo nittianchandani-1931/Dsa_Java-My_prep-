5618787d18f859646fb4b14b283b98a2157b3d7f
Chandani <nittianchandani@gmail.com>
2026-08-16 00:54:17 +0530

    Increasing number triangle pattern using loop

---
+++ Java_DSA/src/Pattern/incrNTriangle.java
@@ -0,0 +1,16 @@
+package Pattern;
+
+public class incrNTriangle {
+
+    public static void main(String[] args) {
+        int n = 5;
+        int count = 1;
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+}
