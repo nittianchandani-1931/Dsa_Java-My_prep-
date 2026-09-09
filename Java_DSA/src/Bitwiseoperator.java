21947ac1e6b17cb2979fa0f69303cb4dbb572a9e
Chandani <nittianchandani@gmail.com>
2026-09-09 12:22:24 +0530

    Implementing different types of bitwise operator

---
+++ Java_DSA/src/Bitwiseoperator.java
@@ -0,0 +1,27 @@
+public class Bitwiseoperator {
+    public static void main() {
+        // int a = 5;
+        // int b = 6;
+        // System.out.println(a & b);
+        // System.out.println(a | b);
+        // System.out.println(a ^ b);
+        // System.out.println(~a);
+
+        // Bitwise left shift <<
+        // int n = 5;
+        // for (int i = 1; i <= 32; i++) {
+        // n = n << 1;
+        // System.out.println(n);
+        // System.out.println();}
+
+        // bitwise right shift >>
+
+        int n = 50;
+        for (int i = 1; i <= 32; i++) {
+            n = n >> 1;
+            System.out.println(n);
+        }
+
+    }
+
+}
