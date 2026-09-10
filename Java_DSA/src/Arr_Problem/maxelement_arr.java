4fa7e1a334518bc4827fb84db720a168e52f7aae
Chandani <nittianchandani@gmail.com>
2026-09-11 00:59:17 +0530

    Finding maximum element in array

---
+++ Java_DSA/src/Arr_Problem/maxelement_arr.java
@@ -0,0 +1,22 @@
+package Arr_Problem;
+
+public class maxelement_arr {
+
+    static int getMaximum(int arr[]) {
+        int maxi = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxi) {
+                maxi = arr[i];
+            }
+        }
+
+        return maxi;
+    }
+
+    static void main() {
+        int arr[] = { 3, 6, 8, 1, 23, 19, 31 };
+        System.out.println(getMaximum(arr));
+    }
+
+}
