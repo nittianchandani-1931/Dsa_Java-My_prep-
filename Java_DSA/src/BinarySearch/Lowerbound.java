068733a2ebdc900038fe8e53b1248155f104b6ab
Chandani <nittianchandani@gmail.com>
2026-09-23 00:58:46 +0530

    Lowerbound in Binarysearch

---
+++ Java_DSA/src/BinarySearch/Lowerbound.java
@@ -0,0 +1,30 @@
+package BinarySearch;
+
+public class Lowerbound {
+    static int getLowerBound(int arr[], int target) {
+        int n = arr.length;
+        int s = 0;
+        int e = n - 1;
+        int ans = -1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (arr[mid] >= target) {
+                // ans store
+                ans = mid;
+                // move to left
+                e = mid - 1;
+            } else {
+                // right move
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    static void main() {
+        int arr[] = { 10, 20, 30, 30, 30, 30, 30, 30, 40, 50 };
+        int target = 90;
+        int ans = getLowerBound(arr, target);
+        System.out.println("Ans: " + ans);
+    }
+}
